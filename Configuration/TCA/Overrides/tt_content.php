4391448c7fb1273aad9a09f1e57d96e4aedebe19
Vladimir Falcón Piva <v.falcon@familie-redllich.de>
2025-09-21 13:28:32 +0200

    Refactor ProposalAppController and ProposalRepository for TYPO3 v13 compatibility; add migration for Cpsit CpsitProposal plugins to content elements

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,27 +1,26 @@
 <?php
 
-declare(strict_types=1);
+defined('TYPO3') or die();
 
 use Cpsit\CpsitProposal\Configuration\SettingsInterface;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
-defined('TYPO3') or die();
-
-(static function (): void {
-    $pluginSignature = strtolower(SettingsInterface::NAME) . '_' . strtolower(SettingsInterface::PLUGIN_APP);
-    ExtensionUtility::registerPlugin(
-        SettingsInterface::NAME,
-        SettingsInterface::PLUGIN_APP,
-        'LLL:EXT:cpsit_proposal/Resources/Private/Language/locallang_be.xlf:app.plugin.title',
-        'icon-proposal-idea',
-        'plugins',
-    );
-
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key,pages,recursive';
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-    ExtensionManagementUtility::addPiFlexFormValue(
-        $pluginSignature,
-        'FILE:EXT:cpsit_proposal/Configuration/FlexForms/AppFlexForm.xml'
-    );
-})();
+$pluginSignature = ExtensionUtility::registerPlugin(
+    SettingsInterface::NAME,
+    SettingsInterface::PLUGIN_APP,
+    'LLL:EXT:cpsit_proposal/Resources/Private/Language/locallang_be.xlf:app.plugin.title',
+    'icon-proposal-idea',
+    'plugins',
+);
+ExtensionManagementUtility::addToAllTCAtypes(
+    'tt_content',
+    '--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.plugin,pi_flexform,',
+    $pluginSignature,
+    'after:palette:headers'
+);
+ExtensionManagementUtility::addPiFlexFormValue(
+    '*',
+    'FILE:EXT:cpsit_proposal/Configuration/FlexForms/AppFlexForm.xml',
+    $pluginSignature
+);
